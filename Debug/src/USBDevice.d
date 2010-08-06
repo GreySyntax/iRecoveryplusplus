df21460a8ae179ce65e1019002416101ef1683c1
Jordan M <jordan@meepmail.co.uk>
2010-08-06 11:54:57 +0100

    Removed /Debug/src and updated .gitignore

--- Debug/src/USBDevice.d
+++
@@ -1,6 +0,0 @@
-src/USBDevice.d src/USBDevice.o: ../src/USBDevice.cpp \
-  ../src/headers/USBDevice.h ../src/headers/Program.h
-
-../src/headers/USBDevice.h:
-
-../src/headers/Program.h:
--- Debug/src/iRecovery.d
+++
@@ -1,6 +0,0 @@
-src/iRecovery.d src/iRecovery.o: ../src/iRecovery.cpp \
-  ../src/headers/Program.h ../src/headers/USBDevice.h
-
-../src/headers/Program.h:
-
-../src/headers/USBDevice.h:
