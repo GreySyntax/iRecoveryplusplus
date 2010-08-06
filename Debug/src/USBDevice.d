a8231e8bae4654f6410fecf47821bc76f6bae11e
Jordan M <jordan@meepmail.co.uk>
2010-08-06 11:53:43 +0100

    Added Makefile to /Debug

---
+++ Debug/src/USBDevice.d
@@ -0,0 +1,6 @@
+src/USBDevice.d src/USBDevice.o: ../src/USBDevice.cpp \
+  ../src/headers/USBDevice.h ../src/headers/Program.h
+
+../src/headers/USBDevice.h:
+
+../src/headers/Program.h:
---
+++ Debug/src/iRecovery.d
@@ -0,0 +1,6 @@
+src/iRecovery.d src/iRecovery.o: ../src/iRecovery.cpp \
+  ../src/headers/Program.h ../src/headers/USBDevice.h
+
+../src/headers/Program.h:
+
+../src/headers/USBDevice.h:
