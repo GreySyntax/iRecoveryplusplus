3228ffabd24125dfebeb1d64d694b7907d959a84
GreySyntax <jordan@meepmail.co.uk>
2010-09-29 18:40:11 +0100

    Starting to implement LazyUSB.

--- src/Program.cpp
+++ src/Program.cpp
@@ -1,9 +1,21 @@
-/*
- * Program.cpp
+/***
+ * iRecovery++ libusb based usb interface for iBoot and iBSS
+ * Copyright (C) 2010  GreySyntax
  *
- *  Created on: Sep 1, 2010
- *      Author: jordan
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+ 
 #include <iostream>
 
 using namespace std;
